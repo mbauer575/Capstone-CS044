ea448a5a10ffa5272b8d652d304efa43b3f9502a
mbauer575 <bauermax@oregonstate.edu>
2025-04-16 22:47:18 -0700

    fixed comments

--- submit.bash
+++ submit.bash
@@ -10,20 +10,19 @@
 # Edit above settings as needed. Currently setup for use on the DGXH cluster at OSU.
 
 # setup environemnt variables (edit this to your needs)
-INPUT_DIR=/nfs/stak/users/bauermax/hpc-share/toy-cars-dino/debug-2/toy-car-dataset                  # Input Directory (where raw images are located)
-OUTPUT_DIR=/nfs/stak/users/bauermax/hpc-share/toy-cars-dino/debug-2/toy-car-dataset/labels-2        # Output Directory (where labels will be saved)
-MODEL_TYPE=yolov8s.pt                                                                               # Model Type (ex "yolov8s.pt" or "yolov8n.pt")
-EXPORT_FORMAT=onnx                                                                                  # Export Format (ex "onnx" or ".pt")
+INPUT_DIR=./toy-car-dataset       # Input Directory (where raw images are located)
+OUTPUT_DIR=./labels-2             # Output Directory (where labels will be saved)
+MODEL_TYPE=yolov8s.pt             # Model Type (ex "yolov8s.pt" or "yolov8n.pt")
+EXPORT_FORMAT=onnx                # Export Format (ex "onnx" or ".pt")
 
 # Loads environment for data labeling. 
-# Load necessary modules (adjust for your environment)
 module load python/3.10            # Load Python module (adjust version if needed)
 
 # Create and activate a Python virtual environment (if not already done)
 python3 -m venv labelenv
 source labelenv/bin/activate
 
-# Install required Python packages
+# Install required training Python packages
 pip install -r label-requirements.txt
 
 pip list
@@ -35,14 +34,13 @@ python label.py $INPUT_DIR $OUTPUT_DIR
 deactivate
 
 # Load environment for training.
-# Load necessary modules (adjust for your environment)
 module load python/3.8            # Load Python module (adjust version if needed)
 
 # Create and activate a Python virtual environment (if not already done)
 python3 -m venv trainenv
 source trainenv/bin/activate
 
-# Install required Python packages
+# Install required training Python packages
 pip install -r train-requirements.txt
 
 pip list
