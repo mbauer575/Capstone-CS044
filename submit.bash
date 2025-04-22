5e68a25721d4926a7389bbf11dd22dbf2ee72c7e
mbauer575 <bauermax@oregonstate.edu>
2025-04-22 11:38:49 -0700

    refactored codebase and prep bash file for auto augmentations

--- submit.bash
+++ submit.bash
@@ -11,30 +11,51 @@
 
 # setup environemnt variables (edit this to your needs)
 INPUT_DIR=./toy-car-dataset       # Input Directory (where raw images are located)
-OUTPUT_DIR=./labels-2             # Output Directory (where labels will be saved)
+AUGMENT_DIR=./augmented-images       # Augmented Directory (where augmented images will be saved)
+# ADD AUGMENT PARAMETERS HERE
+
+OUTPUT_DIR=./labels               # Output Directory (where labels will be saved)
 MODEL_TYPE=yolov8s.pt             # Model Type (ex "yolov8s.pt" or "yolov8n.pt")
 EXPORT_FORMAT=onnx                # Export Format (ex "onnx" or ".pt")
 
+
+# # Load environment for image augmentation.
+# module load python/3.10
+
+# # Create and activate a Python virtual environment (if not already done)
+# python3 -m venv augmentenv
+# source augmentenv/bin/activate
+
+# # Install required augmentation Python packages
+# pip install -r augment-requirements.txt
+
+# pip list
+
+# # Run augmentation script
+# python augment.py $INPUT_DIR $AUGMENT_DIR $AUGMENT_PARAMS
+
+# deactivate
+
+
 # Loads environment for data labeling. 
-module load python/3.10            # Load Python module (adjust version if needed)
+module load python/3.10
 
 # Create and activate a Python virtual environment (if not already done)
 python3 -m venv labelenv
 source labelenv/bin/activate
 
-# Install required training Python packages
+# Install required labeling Python packages
 pip install -r label-requirements.txt
 
 pip list
 
-# Run your Python YOLO training script
+# Run your Python YOLO labeling script
 python label.py $INPUT_DIR $OUTPUT_DIR
 
-# Deactivate the virtual environment
 deactivate
 
 # Load environment for training.
-module load python/3.8            # Load Python module (adjust version if needed)
+module load python/3.8
 
 # Create and activate a Python virtual environment (if not already done)
 python3 -m venv trainenv
@@ -48,5 +69,4 @@ pip list
 # Run your Python YOLO training script
 python train.py $OUTPUT_DIR $MODEL_TYPE $EXPORT_FORMAT
 
-# Deactivate the virtual environment
 deactivate
