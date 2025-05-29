93f215af7539764c72b1d909fc28b3583219d69e
ctf89 <friedmac@oregonstate.edu>
2025-05-29 11:20:34 -0700

    Whoops for real this time

--- display/lcd_sketch/lcd_sketch.ino
+++ display/lcd_sketch/lcd_sketch.ino
@@ -4,6 +4,8 @@ LiquidCrystal lcd(12,11,5,4,3,2);
 const byte numChars=3;
 char receivedChars[numChars];
 bool newData=false;
+int num_spots=0;
+int num_cars=0;
 
 void setup() {
   Serial.begin(9600);
@@ -24,16 +26,23 @@ void recv(){
   while(Serial.available()>0 && newData==false){
     rc=Serial.read();
 
-    if(rc!='\n'){
+    if(rc!='\n' && rc!='@'){
       receivedChars[ndx]=rc;
       ndx++;
       if(ndx>=numChars){
         ndx=numChars-1;
       }
-    }else{
+    }else if(rc=='@'){
       receivedChars[ndx]='\0';
       ndx=0;
       newData=true;
+      num_spots=atoi(receivedChars);
+      Serial.read(); //Clear the \n out of the input buffer
+    }else if(rc=='\n'){
+      receivedChars[ndx]='\0';
+      ndx=0;
+      newData=true;
+      num_cars=atoi(receivedChars);
     }
   }
 }
@@ -42,9 +51,18 @@ void showNewData(){
   if(newData==true){
     lcd.clear();
     lcd.setCursor(0,0);
-    lcd.print("Spots available");
+    int available=num_spots-num_cars;
+    lcd.print(available);
+    lcd.print(" spots open");
     lcd.setCursor(0,1);
-    lcd.print(receivedChars);
+    double percent;
+    if(num_spots!=0){
+      percent=((double)num_cars/(double)num_spots)*100;
+      String convert=String(percent,1);
+      Serial.print(percent);
+      lcd.print(convert);
+      lcd.print("% full");
+    }
     newData=false;
   }
 }
